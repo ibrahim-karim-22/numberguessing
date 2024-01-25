29135f931e9b14e8165cd06b0bc71bfe10c7bd4a
karim <hema_thechamp@yahoo.com>
2024-01-25 17:25:04 -0500

    time to stylize

--- js/number-guessing-game.js
+++ js/number-guessing-game.js
@@ -1,5 +1,6 @@
 document.querySelector("#gameBtn").addEventListener('click', runGame);
 
+
 function runGame(){
   const target = Math.floor(Math.random() * 100) + 1;
 
@@ -7,19 +8,49 @@ let guessString = '';
 let guessNumber = 0;
 let correct = false;
 let numTries = 0;
+let guesses = [];
+//let incorrectGuessCount = 0;
+let hintThreshold = 3; // Set the threshold after which a hint should be shown
 
 do {
+    if(numTries % hintThreshold === 0 && numTries > 3 && numTries < 10) {
+     if (target % 2 === 0) {
+         // Provide a hint based on the current number of tries
+         alert("Hint: The number is even.");
+     } else {
+         alert("Hint: The number is odd.");
+     }
+    }   
     guessString = prompt('I am thinking of a number in the range 1 to 100.\n\nWhat is the number?')
     if (guessString === null) {
         return;
     }
+
     guessNumber = +guessString;
     numTries += 1;
     correct = checkGuess(guessNumber, target);
+//player guesses history//
+    guesses.push(guessNumber);
+    alert("Your guesses so far: " + guesses.join(","));
 } while (!correct);
     alert('You got it! The number was ' + target + '\n\nIt took you ' + numTries + ' tries to guess correctly.');
     
 }
+//odd or even hints//
+/*if (!correct) {
+    incorrectGuessCount++;
+    if (incorrectGuessCount === 3) {  // The threshold for providing a hint
+        let hint = target % 2 === 0 ? "Hint: the number is even." : "Hint: the number is odd.";
+        alert(hint);
+    }
+}
+
+numTries++;*/
+//another way for odd or even hints//
+
+    
+
+
 function checkGuess(guessNumber, target){
     let correct = false;
 
@@ -36,3 +67,5 @@ function checkGuess(guessNumber, target){
     }
     return correct;
 }
+
+
